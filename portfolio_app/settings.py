e362d710521e235a853732a7a78146cb6fdaac5c
Clinton wambugu <clintonwambugu@gmail.com>
2022-08-01 22:48:31 +0300

    Add projects app to installed apps

--- portfolio_app/settings.py
+++ portfolio_app/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'projects.app.ProjectsConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'projects.apps.ProjectsConfig'
 ]
 
 MIDDLEWARE = [
